428df146bddcd0eae33939bc35caf34b6a4ab5d2
songyu <song8328787@163.com>
2017-06-08 23:13:00 +0800

    11

---
+++ backend/config/main-local.php
@@ -0,0 +1,35 @@
+<?php
+// \kartik\grid\Module
+$config = [
+    'bootstrap' => ['rest-client'],
+    'modules' => [
+        'rest-client' => [
+            'class' => 'zhuravljov\yii\rest\Module', 
+            'baseUrl' => 'http://oa.com',
+        ],
+        'gridview' => [
+            'class' => 'kartik\grid\Module',
+        ],
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'wAgvkmDyhaqJt1MrxBHrW0idPbRYkakF',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+    
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
